8294ef6c62128c499d080ce2689c8556e271e880
Samuel A. Rebelsky <rebelsky@grinnell.edu>
2014-02-03 09:44:51 -0600

    Add test framework.

---
+++ src/taojava/labs/classes/FractionTest.java
@@ -0,0 +1,19 @@
+package taojava.labs.classes;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * A test of our fraction class.
+ */
+public class FractionTest
+{
+
+  @Test
+  public void
+    test ()
+  {
+    // STUB
+  } // test
+} // class FractionTest
